9bc58b4af2acb3d1ba3ecbc613df1ea2471b2aa4
sacujo <sacujo93@yandex.ru>
2023-11-11 14:55:06 +0300

    Initial Commit

---
+++ DiceWithCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceWithCode
+//
+//  Created by Igor Guryan on 11.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
